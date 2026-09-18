3d943306af5e339a64928f35fa8a3d0f49a8e767
heart <66947260+bampudding@users.noreply.github.com>
2026-09-18 21:16:45 +0900

    f2fs: drop stale writeback retry label

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2311,7 +2311,6 @@ static int f2fs_write_cache_pages(struct address_space *mapping,
 		tag = PAGECACHE_TAG_TOWRITE;
 	else
 		tag = PAGECACHE_TAG_DIRTY;
-retry:
 	if (wbc->sync_mode == WB_SYNC_ALL || wbc->tagged_writepages)
 		tag_pages_for_writeback(mapping, index, end);
 	done_index = index;
